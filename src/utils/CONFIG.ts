3e0722195e51a1e36e290afb9138656edd227e75
jedwardrico <jacobedwardrico@gmail.com>
2024-04-18 22:39:42 -0700

    fix url

--- src/utils/CONFIG.ts
+++ src/utils/CONFIG.ts
@@ -1,3 +1,3 @@
-export const API_URL = "https://rec-server-nf5j.onrender.com/";
+export const API_URL = "https://rec-server-nf5j.onrender.com/foodtrucks";
 export const TEST_URL = "https://data.sfgov.org/resource/jjew-r69b.json";
 export const DEV = process.env.NODE_ENV === "development";
